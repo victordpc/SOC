4899febf680b56ab6bff36e46816c72ebc500314
JaviSese <jsese21@gmail.com>
2018-11-08 15:50:32 +0100

    Generacion aleatoria de la red

---
+++ P02/random_net.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import csv
+import random
+import sys
+
+
+#sys.argv[0]
+
+
+#argv[1] : number of nodes
+#argv[2] : probability of two nodes having a link
+
+nNodes = sys.argv[1]
+
+p = sys.argv[2]
+
+
+linkSource[]
+linkDestination[]
+
+for i in nNodes
+    j = i
+    while j < nNodes
+        if p >= random.uniform(0, 1)
+            linkSource.append(i)
+            linkDestination.append(j)
+        j += 1
+
+
