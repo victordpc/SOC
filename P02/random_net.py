42f421b5d6508078ae5a66cf2972000dc500237e
JaviSese <jsese21@gmail.com>
2018-11-13 15:16:49 +0100

    Random net generation plus creation of .csv files

--- P02/random_net.py
+++ P02/random_net.py
@@ -6,27 +6,55 @@ import csv
 import random
 import sys
 
-
-#sys.argv[0]
-
-
 #argv[1] : number of nodes
-#argv[2] : probability of two nodes having a link
+#argv[2] : probability of two nodes having an edge
+#argv[3] : path name for the csv
+
 
 nNodes = sys.argv[1]
-
 p = sys.argv[2]
+PATHNODE = '%sNodes.csv'%(argv[3])
+PATHEDGE = '%sEdges.csv'%(argv[3])
+SEPARATOR = ','
+HEADER_NODES = 'Id' + '\n'
+HEADER_EDGE =  'Id' + SEPARATOR +'Source'+ SEPARATOR + 'Target' + '\n'
 
 
-linkSource[]
-linkDestination[]
+#Calculating number of edges with nNodes nodes and p probability
+edgeSource[]
+edgeTarget[]
 
 for i in nNodes
     j = i
     while j < nNodes
         if p >= random.uniform(0, 1)
-            linkSource.append(i)
-            linkDestination.append(j)
-        j += 1
+            edgeSource.append(i)
+            edgeTarget.append(j)
+		j += 1
+
+#Debug
+print 'Calculating edges OK'
+#Generation of the node file .csv
+fNode = open(PATHNODE,'w')
+fNode.write(HEADER_NODES + '\n')
+n = 0
+while n < nNodes
+	fNode.write(n + '\n')
+	n += 1
+fNode.close()
+#Debug
+print 'Generation of the node file: %s OK' % (PATHNODE)
+#Generation of the edges file .csv
+fEdge = open(PATHEDGE,'w')
+fEdge.write(HEADER_EDGE +'\n')
+for n in edgeSource
+	fEdge.write(n + SEPARATOR + edgeSource[n] + SEPARATOR + edgeTarget[n] + '\n')
+fEdge.close()
+#Debug
+print 'Generation of the edge file: %s OK' % (PATHEDGE)
+
+
+
+
 
 
