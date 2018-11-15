be125350ca7b9e39d1a95be8fbf4e662e3237e26
JaviSese <jsese21@gmail.com>
2018-11-15 15:15:42 +0100

    Working version!

--- P02/random_net.py
+++ P02/random_net.py
@@ -11,50 +11,49 @@ import sys
 #argv[3] : path name for the csv
 
 
-nNodes = sys.argv[1]
-p = sys.argv[2]
-PATHNODE = '%sNodes.csv'%(argv[3])
-PATHEDGE = '%sEdges.csv'%(argv[3])
+nNodes = int(sys.argv[1])
+p = float(sys.argv[2])
+PATHNODE = '%sNodes.csv'%(sys.argv[3])
+PATHEDGE = '%sEdges.csv'%(sys.argv[3])
 SEPARATOR = ','
 HEADER_NODES = 'Id' + '\n'
 HEADER_EDGE =  'Id' + SEPARATOR +'Source'+ SEPARATOR + 'Target' + '\n'
 
-
-#Calculating number of edges with nNodes nodes and p probability
-edgeSource[]
-edgeTarget[]
-
-for i in nNodes
-    j = i
-    while j < nNodes
-        if p >= random.uniform(0, 1)
-            edgeSource.append(i)
-            edgeTarget.append(j)
-		j += 1
-
 #Debug
-print 'Calculating edges OK'
+print 'number of nodes:%d\n ' % (nNodes)
+
+print 'probability: %f\n ' % (p)
+
+print 'path of the node file:%s\n' % (PATHNODE)
+
+print 'path of the edge file:%s\n' % (PATHEDGE)
+
 #Generation of the node file .csv
 fNode = open(PATHNODE,'w')
 fNode.write(HEADER_NODES + '\n')
 n = 0
-while n < nNodes
-	fNode.write(n + '\n')
+while n < nNodes:
+	fNode.write(str(n) + '\n')
 	n += 1
 fNode.close()
 #Debug
 print 'Generation of the node file: %s OK' % (PATHNODE)
-#Generation of the edges file .csv
+
+#Calculating number of edges with nNodes nodes and p probability and generation of edges file .csv
 fEdge = open(PATHEDGE,'w')
 fEdge.write(HEADER_EDGE +'\n')
-for n in edgeSource
-	fEdge.write(n + SEPARATOR + edgeSource[n] + SEPARATOR + edgeTarget[n] + '\n')
+pos = 0
+for i in range(nNodes):
+	j = i
+	while j < nNodes:
+		if(i != j):
+			if p >= random.uniform(0, 1):
+				fEdge.write(str(pos) + SEPARATOR + str(i) + SEPARATOR + str(j) + '\n')
+				pos += 1
+		j += 1
+
 fEdge.close()
 #Debug
 print 'Generation of the edge file: %s OK' % (PATHEDGE)
-
-
-
-
 
 
